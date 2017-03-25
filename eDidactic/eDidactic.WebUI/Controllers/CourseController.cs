cf84d424ea4019dc0c5878819f411d661578f3a0
Rafał Kawalerski <kawalerski.r@gmail.com>
2017-03-25 07:32:08 +0100

    add paging

--- eDidactic/eDidactic.WebUI/Controllers/CourseController.cs
+++ eDidactic/eDidactic.WebUI/Controllers/CourseController.cs
@@ -10,15 +10,19 @@ namespace eDidactic.WebUI.Controllers
     public class CourseController : Controller
     {
         private ICourseRepository repository;
+        public int PageSize = 4;
 
         public CourseController(ICourseRepository courseRepository)
         {
             this.repository = courseRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Courses);
+            return View(repository.Courses
+                .OrderBy(p => p.Id)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
         }
 
     }
