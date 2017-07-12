0ee6a7324c7a8615759bb78f53a9bcbef8628bb4
Rafał Kawalerski <kawalerski.r@gmail.com>
2017-07-12 21:46:59 +0200

    add home controller

---
+++ eDidactic/eDidactic.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eDidactic.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StaticContent(string viewname)
+        {
+            //zwraca widok o  
+            return View(viewname);
+        }
+    }
+}
