c9c6bca4a313fc0b50617f1559404b096d06a2c3
Rafał Kawalerski <kawalerski.r@gmail.com>
2017-07-13 23:09:43 +0200

    coursecontroller edit

--- eDidactic/eDidactic.WebUI/Controllers/CourseController.cs
+++ eDidactic/eDidactic.WebUI/Controllers/CourseController.cs
@@ -4,33 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using eDidactic.Domain.Abstract;
+using eDidactic.Domain.Concrete;
 using eDidactic.WebUI.Models;
 
 namespace eDidactic.WebUI.Controllers
 {
     public class CourseController : Controller
     {
-        private ICourseRepository repository;
+        private EFDbContext db = new EFDbContext();
+        //private ICourseRepository repository;
         public int PageSize = 4;
 
-        public CourseController(ICourseRepository courseRepository)
+       /* public CourseController(ICourseRepository courseRepository)
         {
             this.repository = courseRepository;
-        }
+        }*/
 
         public ViewResult List(int page = 1)
         {
             CoursesListViewModel model = new CoursesListViewModel
             {
-                Courses = repository.Courses
-                    .OrderBy(p => p.Id)
+                Courses = db.Courses
+                    .Where(a => a.IsArchive == 0)
+                    .OrderBy(p => p.CourseId)
                     .Skip((page - 1)*PageSize)
                     .Take(PageSize),
                 PagingInfo = new PagingInfo
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Courses.Count()
+                    TotalItems = db.Courses.Count()
                 }
             };
             return View(model);
