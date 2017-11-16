ac55cabaa67b5b17e071adf6ae4c713f0e212682
Rafał Kawalerski <kawalerski.r@gmail.com>
2017-11-16 22:15:51 +0100

    asp.net identity configuration

---
+++ eDidactic/eDidactic.WebUI/Models/UserData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eDidactic.WebUI.Models
+{
+    public class UserData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+}
